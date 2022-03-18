88a8f0e26bbe30773ff7ea0bb8612831795b3d09
User <frankyeh1114@gmail.com>
2022-03-18 15:49:50 +0800

    second commit

---
+++ src/main/java/frank/Customer.java
@@ -0,0 +1,11 @@
+
+package frank;
+
+public class Customer {
+    String id;
+    int amount;
+    float off = 0.1f;
+    public Customer(String id);
+    this.id = id;
+    this.amount = amount;
+}
